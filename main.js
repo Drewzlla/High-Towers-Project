d6e8cedb77bc08db503407fd94dc7bd1325d2316
Drewzlla <ohitzandrew@gmail.com>
2025-08-24 22:17:04 +0100

    Finally done with main code and resonsive design

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ const swiper = new Swiper(".swiper", {
     spaceBetween: 20,
     loop: true,
 });
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+const navItems = document.querySelectorAll("#nav-links a"); 
+
+
+menuBtn.addEventListener("click", (e) => {
+   
+    navLinks.classList.toggle("open");
+
+   
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+
+navItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        
+        navLinks.classList.remove("open");
+        menuBtnIcon.setAttribute("class", "ri-menu-line");
+    });
+});
